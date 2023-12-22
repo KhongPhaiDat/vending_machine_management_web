9fdb1f6e5647b4058219b15782b039c615b564d7
KhongPhaiDat <115723472+KhongPhaiDat@users.noreply.github.com>
2023-12-22 16:45:11 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,7 @@
+provider "aws" {
+  region = "ap-northeast-1"
+}
+
 terraform {
   backend "remote" {
     # The name of your Terraform Cloud organization.
@@ -14,5 +18,24 @@ terraform {
 resource "null_resource" "example" {
   triggers = {
     value = "A example resource that does nothing!"
+  }
+}
+
+# Init 1 DynamoDB table
+resource "aws_dynamodb_table" "menu_database" {
+  name           = "Menu_database"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "id"
+
+  attribute {
+    name = "id"
+    type = "S"
+  }
+
+  ttl {
+    attribute_name = ""
+    enabled        = false
   }
 }
