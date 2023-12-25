14a35febbc565cd17c92909570bf8096dc5f9974
KhongPhaiDat <115723472+KhongPhaiDat@users.noreply.github.com>
2023-12-25 13:31:54 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,50 +12,6 @@ terraform {
   }
 }
 
-resource "aws_s3_bucket" "terraform_state" {
-  bucket = "terraform-state-storage-datluyendevops"
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_versioning" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-  versioning_configuration {
-    status = "Enabled"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
-  bucket = aws_s3_bucket.terraform_state.id
-
-  rule {
-    apply_server_side_encryption_by_default {
-      sse_algorithm = "AES256"
-    }
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
-resource "aws_dynamodb_table" "terraform_lock_state" {
-  name         = "terraform-lock-state"
-  billing_mode = "PAY_PER_REQUEST"
-  hash_key     = "LockID"
-
-  attribute {
-    name = "LockID"
-    type = "S"
-  }
-  lifecycle {
-    prevent_destroy = true
-  }
-}
-
 # Init 1 DynamoDB table
 resource "aws_dynamodb_table" "menu_database" {
   name           = "Menu_database"
