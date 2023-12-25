15b18c0480bfc20304f24632d821ae5d3cdda0c9
KhongPhaiDat <115723472+KhongPhaiDat@users.noreply.github.com>
2023-12-25 12:20:58 +0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,28 +4,45 @@ provider "aws" {
 
 terraform {
   backend "s3" {
-    bucket = "terraform-state-storage-datluyendevops"
-    key    = "test"
-    region = "ap-northeast-1"
+    bucket         = "terraform-state-storage-datluyendevops"
+    key            = "vending-machine-management/terraform.tfstate"
+    region         = "ap-northeast-1"
+    dynamodb_table = "terraform-lock-state"
+    encrypt        = true
   }
 }
 
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "terraform-state-storage-datluyendevops"
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
-# Init 1 DynamoDB table
-resource "aws_dynamodb_table" "menu_database" {
-  name           = "test_database"
-  billing_mode   = "PROVISIONED"
-  read_capacity  = 1
-  write_capacity = 1
-  hash_key       = "id"
+resource "aws_s3_bucket_versioning" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "terraform_state" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock_state" {
+  name         = "terraform-lock-state"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
 
   attribute {
-    name = "id"
+    name = "LockID"
     type = "S"
-  }
-
-  ttl {
-    attribute_name = ""
-    enabled        = false
   }
 }
